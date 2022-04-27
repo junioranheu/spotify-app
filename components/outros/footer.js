93afcdb3ec48224e6f0283cd3bcf578ae74743bc
junioranheu <junioranheu@gmail.com>
2022-04-27 18:28:26 -0300

    importar UsuarioContext em footer.js para verificar se o usuário está logado ou não

--- components/outros/footer.js
+++ components/outros/footer.js
@@ -1,13 +1,16 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import Styles from '../../css/footer';
+import { UsuarioContext } from '../../utils/context/usuarioContext';
 import Casa from '../svg/casa';
 import Fila from '../svg/fila';
 import Lupa from '../svg/lupa';
+import SpotifyLogo2 from '../svg/spotifyLogo2';
 
 export default function Footer({ rotaAtual }) {
     const navigation = useNavigation();
+    const [usuarioContext] = useContext(UsuarioContext); // Contexto do usuário;
 
     return (
         <View style={Styles.footer}>
@@ -23,12 +26,23 @@ export default function Footer({ rotaAtual }) {
                 <Text style={Styles.texto}>Buscar</Text>
             </View>
 
-            <TouchableOpacity onPress={() => navigation.navigate('Fila')}>
-                <View style={Styles.divIcone}>
-                    <Fila height={24} width={24} cor={rotaAtual === 'Fila' ? 'rgba(255, 255, 255, 0.9)' : 'rgba(255, 255, 255, 0.5)'} />
-                    <Text style={Styles.texto}>Sua fila</Text>
-                </View>
-            </TouchableOpacity>
+            {
+                usuarioContext?.length > 0 ? (
+                    <TouchableOpacity onPress={() => navigation.navigate('Fila')}>
+                        <View style={Styles.divIcone}>
+                            <Fila height={24} width={24} cor={rotaAtual === 'Fila' ? 'rgba(255, 255, 255, 0.9)' : 'rgba(255, 255, 255, 0.5)'} />
+                            <Text style={Styles.texto}>Sua fila</Text>
+                        </View>
+                    </TouchableOpacity>
+                ) : (
+                    <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                        <View style={Styles.divIcone}>
+                            <SpotifyLogo2 height={24} width={24} cor={rotaAtual === 'Login' ? 'rgba(255, 255, 255, 0.9)' : 'rgba(255, 255, 255, 0.5)'} />
+                            <Text style={Styles.texto}>Entrar</Text>
+                        </View>
+                    </TouchableOpacity>
+                )
+            }
         </View>
     );
 }
--- views/app.js
+++ views/app.js
@@ -26,6 +26,7 @@ export default function App() {
     const [rotaAtual, setRotaAtual] = useState(rotaInicial);
     StatusBar.setBarStyle('light-content', true); // Alterar a cor do StatusBar: https://stackoverflow.com/questions/39297291/how-to-set-ios-status-bar-background-color-in-react-native;
 
+    // Toast/Aviso.js;
     const toastConfig = {
         success: (props) => (
             <BaseToast
